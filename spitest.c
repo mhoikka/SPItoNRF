0996169bb36fb891314210485042df540e32d5fd
mhoikka <mhoikka@asu.edu>
2024-10-25 21:06:32 -0500

    Remove debug lines

--- spitest.c
+++ spitest.c
@@ -161,9 +161,7 @@ void receiveByteNRF(unsigned char data){
     unsigned char rxAddress[3] = {0x93, 0xBD, 0x6B}; // Variable to hold the RX address
 
     //set control registers
-    readwriteNRF_SPI(SETUP_AW, &dummy, 1, READ_REG_NRF); //read address width register
     readwriteNRF_SPI(SETUP_AW, &addressWidth, 1, WRITE_REG_NRF); //set to 3 byte address width
-    readwriteNRF_SPI(SETUP_AW, &dummy, 1, READ_REG_NRF); //read address width register
     readwriteNRF_SPI(RX_ADDR_P01, &rxAddress, 3, WRITE_REG_NRF); //set read address
     readwriteNRF_SPI(RF_SETUP, &rfSetup, 1, WRITE_REG_NRF); //set RF Data Rate to 250kbps, RF output power to -18dBm
     //write data to be transmitted into TX FIFO
