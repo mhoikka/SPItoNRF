3c5fd102ea2546875cc01387099c7148413bacb9
mhoikka <mhoikka@asu.edu>
2024-10-26 17:40:20 -0500

    Test

--- spitest.c
+++ spitest.c
@@ -1,65 +1,3 @@
-/******************************************************************************
-spitest.cpp
-Raspberry Pi SPI interface demo
-Byron Jacquot @ SparkFun Electronics>
-4/2/2014
-https://github.com/sparkfun/Pi_Wedge
-
-A brief demonstration of the Raspberry Pi SPI interface, using the SparkFun
-Pi Wedge breakout board.
-
-Resources:
-
-This example makes use of the Wiring Pi library, which streamlines the interface
-to the the I/O pins on the Raspberry Pi, providing an API that is similar to the
-Arduino.  You can learn about installing Wiring Pi here:
-https://github.com/WiringPi/WiringPi/releases
-
-The wiringPi SPI API is documented here:
-https://projects.drogon.net/raspberry-pi/wiringpi/spi-library/
-
-The init call returns a standard file descriptor.  More detailed configuration
-of the interface can be performed using ioctl calls on that descriptor.
-See the wiringPi SPI implementation (wiringPi/wiringPiSPI.c) for some examples.
-Parameters configurable with ioctl are documented here:
-http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/tree/Documentation/spi/spidev
-
-Hardware connections:
-
-This file interfaces with the SparkFun Serial 7 Segment display:
-https://www.sparkfun.com/products/11629
-
-The board was connected as follows:
-(Raspberry Pi)(Serial 7 Segment)
-GND  -> GND
-3.3V -> Vcc
-CE1  -> SS (Shift Select)
-SCK  -> SCK 
-MOSI -> SDI
-MISO -> SDO
-
-To build this file, I use the command:
->  g++ spitest.cpp -lwiringPi
-
-Then to run it, first the spi kernel module needs to be loaded.  This can be 
-done using the GPIO utility.
-> gpio load spi
-> ./a.out
-
-This test uses the single-segment mode of the 7 segment display.  It shifts a 
-bit through the display characters, lighting a single character of each at a 
-time.
-
-Development environment specifics:
-Tested on Raspberry Pi V2 hardware, running Raspbian.
-Building with GCC 4.6.3 (Debian 4.6.3-14+rpi1)
-
-This code is beerware; if you see me (or any other SparkFun employee) at the
-local, and you've found our code helpful, please buy us a round!
-
-Distributed as-is; no warranty is given.
-******************************************************************************/
-
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -87,12 +25,6 @@ unsigned char READ_PAYLOAD_NRF = 0x60;
 unsigned char FLUSH_TX_NRF = 0xE1;
 unsigned char FLUSH_RX_NRF = 0xE2;
 
-//WiringPi constants
-//unsigned char HIGH = 1;
-//unsigned char LOW = 0;
-//unsigned char OUTPUT = 1;
-//unsigned char INPUT = 0;
-
 int main()
 {
     int fd, result;
@@ -118,6 +50,7 @@ int main()
 
     pinMode(2, OUTPUT); //set CE pin to output
     pinMode(3, INPUT); //set IRQ pin to input
+    digitalWrite(2, LOW); 
     //pullUpDnControl(3, PUD_UP); //enable pull-up resistor on IRQ pin
 
     delay(200); //give the chip time to power on
