ec9c605ccbf87e91285a99b5d9434ecf08b74813
mhoikka <mhoikka@asu.edu>
2024-11-16 12:24:04 -0600

    Add ack functionality

--- spitest.c
+++ spitest.c
@@ -181,8 +181,8 @@ void receiveByteNRF(){
     readwriteNRF_SPI(STATUS, &clear_irqrx, 1, WRITE_REG_NRF); 
     readwriteNRF_SPI(STATUS, &clear_irqtx, 1, WRITE_REG_NRF); 
 
-    //my_delay(2000);
-    while(!(STATUS & (1 << 6))); //wait for data to be received
+    my_delay(2000);
+    //while(!(STATUS & (1 << 6))); //wait for data to be received
     readwriteNRF_SPI(0x00, buffer, 32, READ_PAYLOAD_NRF); //read data from RX FIFO
     //printf("Data received : %d\n", buffer[0]);
     printBuffer(buffer, 32); //see what's in that buffer
