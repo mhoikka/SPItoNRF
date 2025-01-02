2e7254fb3a723b8c4d26c89390ca99b4803307f6
mhoikka <mhoikka@asu.edu>
2025-01-01 22:01:51 -0600

    Pipe address issue use hex

--- spitest.c
+++ spitest.c
@@ -140,8 +140,7 @@ void receiveByteNRF(){
 
     readwriteNRF_SPI(SETUP_AW, &ADDRESS_WIDTH, 1, WRITE_REG_NRF, 0); //set to 3 byte address width
 
-    unsigned char enabledPipes = 0x3F; //each 1 in the binary representation of this number corresponds to an enabled pipe in range pipes 0-5
-    //0x3F enable all pipes in the 'enabledPipes' variable
+    unsigned char enabledPipes = 0x03; //each 1 in the binary representation of this number corresponds to an enabled pipe in range pipes 0-5
     enableDataPipes(enabledPipes);
 
     readwriteNRF_SPI(RF_SETUP, &RFSETUP, 1, WRITE_REG_NRF, 0); //set RF Data Rate to 2Mbps, RF output power to 0dBm
