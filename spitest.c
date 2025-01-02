0ef7ead352df9b86804448baa8969d68901f9444
mhoikka <mhoikka@asu.edu>
2025-01-01 22:04:28 -0600

    Pipe address offset

--- spitest.c
+++ spitest.c
@@ -221,7 +221,7 @@ void enableDataPipes(unsigned char pipes){
                 if (pipe == 0x01){
                     rxAddress[0] = 0x93; //unique address for pipe 0
                     rxAddress[1] = 0xBD;
-                    rxAddress[2] = 0x6B + pipe; //increment the address by the pipe number to ensure unique addresses for each pipe
+                    rxAddress[2] = 0x6A + pipe; //increment the address by the pipe number to ensure unique addresses for each pipe
                     readwriteNRF_SPI(RX_ADDR_Px, rxAddress, 3, WRITE_REG_NRF, 0); //set read address for pipe
                 }
                 else{
