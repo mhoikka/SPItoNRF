65da14121bad8b30b737f3cae451815671b95183
mhoikka <mhoikka@asu.edu>
2025-01-01 22:22:42 -0600

    Correct seg fault logic

--- spitest.c
+++ spitest.c
@@ -218,7 +218,7 @@ void enableDataPipes(unsigned char pipes){
                 readwriteNRF_SPI(RX_ADDR_Px, rxAddress, 3, WRITE_REG_NRF, 0); //set read address for pipe
             }
             else{
-                if (1){
+                if (pipe == 0x01){
                     rxAddress[0] = 0x93; //unique address for pipe 0
                     rxAddress[1] = 0xBD;
                     rxAddress[2] = 0x6A + pipe; //increment the address by the pipe number to ensure unique addresses for each pipe
@@ -226,7 +226,8 @@ void enableDataPipes(unsigned char pipes){
                 }
                 else{
                     rxAddress[2] = 0x6B + pipe; //increment the address by the pipe number to ensure unique addresses for each pipe
-                    readwriteNRF_SPI(RX_ADDR_Px, rxAddress[2], 1, WRITE_REG_NRF, 0); //set read address for pipe
+                    unsigned char oneByteAddress = rxAddress[2];
+                    readwriteNRF_SPI(RX_ADDR_Px, &oneByteAddress, 1, WRITE_REG_NRF, 0); //set read address for pipe
                 }
             }
 
